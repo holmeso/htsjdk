3a815b3ea1078c09c9df2ed40ab1c19925b6361a
holmeso <holmeso@users.noreply.github.com>
2024-10-29 16:39:09 +1000

    Update ByteArrayStopCodec.java

--- src/main/java/htsjdk/samtools/cram/encoding/external/ByteArrayStopCodec.java
+++ src/main/java/htsjdk/samtools/cram/encoding/external/ByteArrayStopCodec.java
@@ -12,7 +12,9 @@ import java.io.IOException;
  */
 final class ByteArrayStopCodec extends ExternalCodec<byte[]> {
     private final int stop;
-
+    private final PushbackInputStream pushBackInputStream;
+    private static final int BUFFER_SIZE = 152;
+    
     /**
      * Construct a Byte Array Stop Codec
      *
@@ -25,17 +27,30 @@ final class ByteArrayStopCodec extends ExternalCodec<byte[]> {
                               final byte stopByte) {
         super(inputStream, outputStream);
         this.stop = 0xFF & stopByte;
+        pushBackInputStream = new PushbackInputStream(inputStream, BUFFER_SIZE);
     }
 
     @Override
     public byte[] read() {
-        final ByteArrayOutputStream readingBAOS = new ByteArrayOutputStream();
-        int b;
-        readingBAOS.reset();
-        while ((b = inputStream.read()) != -1 && b != stop) {
-            readingBAOS.write(b);
+        final ByteArrayOutputStream readingBAOS = new ByteArrayOutputStream(BUFFER_SIZE);
+        byte [] buffer = new byte[BUFFER_SIZE];
+        int numberOfBytesRead;
+        try {
+            while ((numberOfBytesRead = pushBackInputStream.read(buffer)) != -1) {
+                for (int i = 0; i < numberOfBytesRead; i++) {
+                    if ((buffer[i] & 0xFF) == stop) {
+                        // Write all bytes up to the stop byte
+                        readingBAOS.write(buffer, 0, i);
+                        // Unread the remaining bytes (i+1 to bytesRead)
+                        pushBackInputStream.unread(buffer, i + 1, numberOfBytesRead - (i + 1));
+                        return readingBAOS.toByteArray();
+                    }
+                }
+                readingBAOS.write(buffer, 0, numberOfBytesRead);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
         return readingBAOS.toByteArray();
     }
 
